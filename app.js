2058e53f4a5bc167f2a73781bf31a31814d1ad1c
Ken Eucker <keneucker@gmail.com>
2018-04-17 14:03:15 -0700

    First commit, this site is using the template from @ajlkn.

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express'),
+    path = require('path'),
+    app = express(),
+    favicon = require('serve-favicon'),
+    port = 8080;
+
+console.log(path.join(__dirname, '/templates/pdxbikesafe/'));
+app.use(express.static(path.join(__dirname, '/templates/pdxbikesafe/')));
+app.use(favicon(path.join(__dirname, 'assets/', 'favicon.ico')));
+
+app.use("/assets", function(req, res) {
+    var file = req.url = (req.url.indexOf('?') != -1) ? req.url.substring(0, req.url.indexOf('?')) : req.url;
+    res.sendFile(path.join(__dirname, "assets/", req.url));
+});
+
+app.listen(port, function () {
+    console.log("App listening on: http://localhost:" + port);
+});
