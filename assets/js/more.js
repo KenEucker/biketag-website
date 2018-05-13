fadb05883f0072053ea7b19967b95d63742f2bba
Ken Eucker <keneucker@gmail.com>
2018-05-13 14:04:29 -0700

    Wrapped up the imgur integration into it's own object.

--- assets/js/more.js
+++ assets/js/more.js
@@ -1,29 +1,48 @@
 (function($) {
-    var imgurAlbumHash = '3OMZJ6a';
-    var imgurAuthorization = 'Client-ID 79ea70333c45883';
 
-    function showLatestTagImages()  {
-        $.ajax({
-            url: 'https://api.imgur.com/3/album/' + imgurAlbumHash + '/images',
-            success: function(data) {
-                console.log(data);
-                var images = data.data;
-                var lastImage = images[images.length - 1];
-                var secondToLastImage = images.length > 1 ? images[images.length - 2] : null;
+    var imgurIntegration = {
+        imgurAlbumHash: '3OMZJ6a',
+        imgurAuthorization: 'Client-ID 79ea70333c45883',
+        imgurAlbumPictures: null,
 
-                $('.content .inner').append( $('<img src="' + lastImage.link + '">') );
-                if(secondToLastImage) {
-                    $('.content .inner').append( $('<img src="' + secondToLastImage.link + '">') );
-                }
-            },
-            error: function(err) {
-                console.log('error getting images from imgur', err);
-            },
-            beforeSend: function (xhr) {
-                xhr.setRequestHeader ("Authorization", imgurAuthorization);
-            },
-        });
-    }
+        getImgurAlbumPictures: function(hash, callback)  {
+            if (!hash) {
+                hash = this.imgurAlbumHash;
+            }
+            $.ajax({
+                url: 'https://api.imgur.com/3/album/' + hash + '/images',
+                success: function(data) {
+                    console.log(data);
+                    window.imgurIntegration.imgurAlbumPictures = data.data;
 
-    window.showLatestTagImages = showLatestTagImages;
+                    if (callback) {
+                        callback(data);
+                    }
+                },
+                error: function(err) {
+                    console.log('error getting images from imgur', err);
+                },
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader ("Authorization", window.imgurIntegration.imgurAuthorization);
+                },
+            });
+        },
+
+        showLatestTagImages: function() {
+            if (!window.imgurIntegration.imgurAlbumPictures) {
+                window.imgurIntegration.getImgurAlbumPictures(null, window.imgurIntegration.showLatestTagImages);
+            }
+
+            var images = window.imgurIntegration.imgurAlbumPictures;
+            var lastImage = images[images.length - 1];
+            var secondToLastImage = images.length > 1 ? images[images.length - 2] : null;
+
+            $('.content .inner').append( $('<img src="' + lastImage.link + '">') );
+            if(secondToLastImage) {
+                $('.content .inner').append( $('<img src="' + secondToLastImage.link + '">') );
+            }
+        }
+    };
+
+    window.imgurIntegration = imgurIntegration;
 })(jQuery);
