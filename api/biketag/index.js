b04d6805f2eb78939edca4efe1e99f54645dd3a1
Ken Eucker <keneucker@gmail.com>
2020-07-05 16:18:44 -0700

    Fixed the reddit autoposting and added the email notification. Also resolved email sending in sexpress.

--- api/biketag/index.js
+++ api/biketag/index.js
@@ -7,6 +7,27 @@ const ejs = require('ejs')
 const fs = require('fs')
 
 exports.engine = 'ejs';
+let _app, _appConfig
+
+
+const sendEmailToAllAdministrators = async (config, subject, body, latestTagInfo) => {
+	const emailPromises = []
+
+	if ((!subject && !body) && !latestTagInfo) {
+		await getTagInformation(config, 'latest', config.imgur.imgurAlbumHash, (t) => latestTagInfo = t)
+	}
+	const latestTagNumber = !!latestTagInfo ? latestTagInfo.latestTagNumber : '-'
+	subject = subject || `New Bike Tag Post (#${latestTagNumber}) [${config.requestSubdomain}]`
+	body = body || `Hello BikeTag Admin, this email is a confirmation that the most recent tag, number ${latestTagNumber}, has been autposted to reddit by BikeTag.Org.\r\n\r\nYou are getting this email because you are listed as an admin on the site (${config.host}).\r\n\r\nReply to this email to request to be removed from this admin list.`
+
+	config.adminEmailAddresses.forEach((emailAddress) => {
+		emailPromises.push(_app.sendEmail(config, {to: emailAddress, subject, text: body, callback: (info) => {
+			// console.log(`email sent to ${emailAddress}`, info)
+		}}).catch(e => console.log({emailSendError: e})))
+	})
+
+	return Promise.all(emailPromises)
+}
 
 const getTagInformation = (config, tagNumber, albumHash, callback) => {
 	// console.trace({config})
@@ -65,44 +86,67 @@ const getTagInformation = (config, tagNumber, albumHash, callback) => {
 
 const postLatestBikeTagToReddit = (config, callback) => {
 	const redditOpts = {
-		userAgent: config.reddit.redditUserAgent.replace('VERSION', config.version),
+		userAgent: config.reddit.redditUserAgent.replace('VERSION', config.version).replace('biketag.org', config.host),
 		clientId: config.reddit.redditClientID,
 		clientSecret: config.reddit.redditClientSecret,
 		username: config.reddit.redditUserName,
 		password: config.reddit.redditPassword,
-		// accessToken: `bearer ${config.reddit.redditAccessToken}`
+		accessToken: `bearer ${config.reddit.redditAccessToken}`
 	}
 
-	// console.log({redditOpts, redditConfig: config.reddit, config})
-
-	const r = new reddit(redditOpts)
+	let r = new reddit(redditOpts)
 
 	return getTagInformation(config, config.latestTagNumber, config.imgur.imgurAlbumHash, async (tagData) => {
 		tagData.host = config.host
 		const redditTemplatePath = `${config.viewsFolder}/reddit/post.ejs`
 		const redditTemplateString = fs.readFileSync(redditTemplatePath, 'utf-8')
 		const latestTagTemplate = ejs.render(redditTemplateString, tagData).replace('<pre>', '').replace('</pre>', '')
-		// console.log({tagData, latestTagTemplate, reddit: config.reddit})
-	
-		r.getSubreddit(config.reddit.redditSubreddit).submitSelfpost({
-			title: `Bike Tag #${config.latestTagNumber}`,
-			text: latestTagTemplate,
-		}).sticky().distinguish().approve().then((redditData) => {
-			if (!redditData.name) {
-				console.log('Error creating self post to reddit', redditOpts)
-				return callback()
-			}
-
-			r.getSubmission(redditData.name).fetch().then(submission => {
-				if (!!submission) {
-					r.getSubreddit('biketag').submitLink({
-						title: `[X-Post r/${config.reddit.redditSubreddit}] Bike Tag #${config.latestTagNumber} (${config.region})`,
-						url: submission.url
-					}).then(callback)
-				}
-			  })
-		}).catch(callback)
+		const regionName = config.requestSubdomain.charAt(0).toUpperCase() + config.requestSubdomain.slice(1)
+		// console.log({tagData, latestTagTemplate, config})
+		
+		return sendEmailToAllAdministrators(config).then(callback)
 	})
+	// 	return r.getSubreddit(config.reddit.redditSubreddit)
+	// 		.submitSelfpost({
+	// 			title: `Bike Tag #${config.latestTagNumber}`,
+	// 			text: latestTagTemplate,
+	// 		})
+	// 		.assignFlair({text: config.reddit.postFlair || 'BikeTag'})
+	// 		.approve()
+	// 		.sticky()
+	// 		.distinguish()
+	// 		.submitCrosspost({
+	// 			subredditName: 'biketag',
+	// 			title: `[X-Post r/${config.reddit.redditSubreddit}] Bike Tag #${config.latestTagNumber} (${config.region})`,
+	// 			resubmit: false,
+	// 		})
+	// 		// .assignFlair({text: regionName})
+	// 		.then((redditData) => {
+	// 			if (!redditData.name) {
+	// 				console.log('Error creating self post to reddit', redditOpts)
+	// 				return callback()
+	// 			}
+
+	// 			// TODO: send emails of successful posting to reddit
+
+	// 			redditOpts.username = _appConfig.defaults.redditUserName
+	// 			redditOpts.password = _appConfig.defaults.redditPassword
+
+	// 			r = new reddit(redditOpts)
+
+	// 			/// TODO: Make this submission response handled by the u/biketagorg account for assigning
+	// 			r.getSubmission(redditData.name).fetch().then(submission => {
+	// 				if (!!submission) {
+	// 					return r.getSubreddit(_appConfig.defaults.redditSubreddit)
+	// 						.assignFlair({text: regionName})
+	// 						.then(callback)
+	// 						.catch(callback)
+	// 				} else {
+	// 					console.error('post to reddit error!')
+	// 				}
+	// 			}).catch(callback)
+	// 		}).catch(callback)
+	// })
 }
 
 function getBikeTagNumberFromImage(image) {
@@ -206,7 +250,8 @@ const update = (req, res, next) => {
 }
 
 const routes = (app) => {
-	const appConfig = app.config
+	_app = app
+	_appConfig = app.config
 
 	app.filterSubdomainRequest("/latest", (subdomain, req, res, host) => {
   
@@ -257,16 +302,16 @@ const routes = (app) => {
 	app.filterSubdomainRequest('/post/email', async (subdomain, req, res, host) => {
 		try {
 			const subdomainConfig = app.getSubdomainOpts(req)
-			return getTagInformation(appConfig, 'latest', subdomainConfig.imgur.imgurAlbumHash, (latestTagInfo) => {
+			return getTagInformation(subdomainConfig, 'latest', subdomainConfig.imgur.imgurAlbumHash, (latestTagInfo) => {
 				const latestTagNumber = subdomainConfig.latestTagNumber = latestTagInfo.latestTagNumber
 				const subject = `New Bike Tag Post (#${latestTagNumber}) [${subdomain}]`
 				const body = `Hello BikeTag Admin, A new BikeTag has been posted in ${subdomainConfig.region}!\r\nTo post this tag to Reddit manually, go to ${host}/get/reddit to get the reddit post template.\r\n\r\nYou are getting this email because you are listed as an admin on the site (${host}).\r\n\r\nReply to this email to request to be removed from this admin list.`
 
 				subdomainConfig.adminEmailAddresses.forEach((emailAddress) => {
-					app.sendEmail(subdomainConfig, emailAddress, subject, body, (info) => {
+					app.sendEmail(subdomainConfig, { emailAddress, subject, body, callback: (info) => {
 						console.log(`email sent to ${emailAddress}`, info)
 						// res.json(JSON.stringify(info))
-					})
+					}})
 				})
 			})
 		} catch (error) {
@@ -283,20 +328,23 @@ const routes = (app) => {
 		const subdomainConfig = app.getSubdomainOpts(req)
 		subdomainConfig.requestSubdomain = subdomain
 		subdomainConfig.host = host
-		subdomainConfig.viewsFolder = appConfig.viewsFolder
+		subdomainConfig.viewsFolder = _appConfig.viewsFolder
+		subdomainConfig.version = _appConfig.version
 
 		return getTagInformation(subdomainConfig, 'latest', subdomainConfig.imgur.imgurAlbumHash, (latestTagInfo) => {
 			subdomainConfig.latestTagNumber = latestTagInfo.latestTagNumber
 
-			// subdomainConfig.reddit.redditPassword = appConfig.defaults.redditPassword
-			// subdomainConfig.reddit.redditUserName = appConfig.defaults.redditUserName
-
 			return postLatestBikeTagToReddit(subdomainConfig, (response) => {
-				console.log('posted to reddit', response)
+				if (!!response.error) {
+					
+				} else {
+					console.log('posted to reddit', response)
+				}
+
 				res.json({ success: response })
 			})
 		}).catch(e => {
-			console.log({ redditApiError: e })
+				console.log({ redditApiError: e })
 			res.json({ error: e.message })
 		})
 	}, 'post')
