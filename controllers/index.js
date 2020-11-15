70ea25523e5a72b6d9e284e173d240c005a92874
Ken Eucker <keneucker@gmail.com>
2020-11-15 10:53:13 -0800

    Removed an erroneous console log.

--- controllers/index.js
+++ controllers/index.js
@@ -30,7 +30,6 @@ class IndexController {
         const template = this.app.getTemplateNameFromSubdomain(subdomain)
         const data = this.app.getPublicConfig(subdomain, host)
 
-        console.log({ views: this.app.get('views') })
         return this.app.renderTemplate(template, data, res)
     }
 
