a218ef2f161dd313dbdc7a1395324ef19dc58980
Ken Eucker <keneucker@gmail.com>
2020-12-01 12:30:39 -0800

    Removed all of the biketag-app code.

--- config/config.js
+++ config/config.js
@@ -1,69 +1,9 @@
-const { extname } = require('path')
-
-const biketagImage = (url, size = '') => {
-    const ext = extname(url)
-    /// Make sure the image type is supported
-    if (['.jpg', '.jpeg', '.png', '.bmp'].indexOf(ext) === -1) return url
-
-    switch (size) {
-        default:
-        case 'original':
-        case '':
-            break
-
-        case 'small':
-            size = 's'
-        case 's':
-            break
-
-        case 'medium':
-            size = 's'
-        case 'm':
-            break
-
-        case 'large':
-            size = 'l'
-        case 'l':
-            break
-    }
-
-    return url.replace(ext, `${size}${ext}`)
-}
-
 module.exports = {
-    middlewares: { email: true },
-    rendering: {
-        liquid: {
-            customFilters: {
-                biketag_image: biketagImage,
-            },
-        },
+    public: {
+        imgur: ['albumHash', 'queueHash'],
+        reddit: ['subreddit'],
     },
-    ssl: {
-        contentSecurityPolicy: {
-            directives: {
-                fontSrc: [`'self'`, `https://fonts.googleapis.com`, `https://weloveiconfonts.com`],
-                styleSrc: [
-                    `'self'`,
-                    `'unsafe-inline' https://fonts.googleapis.com`,
-                    `'unsafe-inline' uglipop.min.js`,
-                ],
-                scriptSrc: [`'unsafe-inline' https://www.googletagmanager.com`],
-                connectSrc: [
-                    `'self'`,
-                    `https://api.imgur.com`,
-                    `https://www.google-analytics.com`,
-                    `https://mdbootstrap.com`,
-                ],
-                imgSrc: [
-                    `'self'`,
-                    `https://i.imgur.com`,
-                    `https://www.googletagmanager.com`,
-                    `https://www.google-analytics.com`,
-                    `https://mdbootstrap.com`,
-                    `data:`,
-                ],
-            },
-        },
+    rendering: {
+        overrideViewEngine: ['liquid', 'ejs'],
     },
 }
