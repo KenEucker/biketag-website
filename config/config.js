fb1752820274c4cf1ea6d1400de3b59c32df8bf2
Ken Eucker <keneucker@gmail.com>
2020-12-01 21:22:18 -0800

    Added the config.js and set the generate configuration to not copy over the config.js file from the source.

--- config/config.js
+++ config/config.js
@@ -1,14 +1,69 @@
+const { extname } = require('path')
+
+const biketagImage = (url, size = '') => {
+    const ext = extname(url)
+    /// Make sure the image type is supported
+    if (['.jpg', '.jpeg', '.png', '.bmp'].indexOf(ext) === -1) return url
+
+    switch (size) {
+        default:
+        case 'original':
+        case '':
+            break
+
+        case 'small':
+            size = 's'
+        case 's':
+            break
+
+        case 'medium':
+            size = 's'
+        case 'm':
+            break
+
+        case 'large':
+            size = 'l'
+        case 'l':
+            break
+    }
+
+    return url.replace(ext, `${size}${ext}`)
+}
+
 module.exports = {
-	ssl: {
-	},
-	defaults: {
-		template: "biketag1",
-	},
-    public: {
-        imgur: ['albumHash', 'queueHash'],
-        reddit: ['subreddit'],
-    },
+    middlewares: { email: true },
     rendering: {
-        overrideViewEngine: ['liquid', 'ejs'],
-	},
+        liquid: {
+            customFilters: {
+                biketag_image: biketagImage,
+            },
+        },
+    },
+    ssl: {
+        contentSecurityPolicy: {
+            directives: {
+                fontSrc: [`'self'`, `https://fonts.googleapis.com`, `https://weloveiconfonts.com`],
+                styleSrc: [
+                    `'self'`,
+                    `'unsafe-inline' https://fonts.googleapis.com`,
+                    `'unsafe-inline' uglipop.min.js`,
+                ],
+                scriptSrc: [`'unsafe-inline' https://www.googletagmanager.com`],
+                connectSrc: [
+                    `'self'`,
+                    `https://api.imgur.com`,
+                    `https://www.google-analytics.com`,
+                    `https://mdbootstrap.com`,
+                ],
+                imgSrc: [
+                    `'self'`,
+                    `https://i.imgur.com`,
+                    `https://www.googletagmanager.com`,
+                    `https://www.google-analytics.com`,
+                    `https://mdbootstrap.com`,
+                    `data:`,
+                ],
+            },
+        },
+    },
 }
