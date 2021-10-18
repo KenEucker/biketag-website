81ebfee38f35fee6ce63e183cca279f251f2727a
Ken Eucker <keneucker@gmail.com>
2021-10-18 11:45:31 -0700

    Adds the code for notifications with supabase app.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const sexpress = require('sexpress')
+const { createClient } = require('@supabase/supabase-js')
 
 /// BikeTag App specific configuration filters
 const onPageDataRequest = function BikeTagPublicData(publicData, appConfig, subdomain) {
@@ -36,9 +37,42 @@ const appStarted = function BikeTagAppStarted() {}
 
 const onConfigurationLoad = function BikeTagConfigurationLoad(config) {}
 
+const onLoad = async function onLoad(config) {	
+	// Create a single supabase client for interacting with your database
+	const supabase = createClient('localhost:5555', config.notifications?.publicKey)
+
+	app.hook('notifications:subscribe', async (subscription) => {
+		const { data, error } = await supabase
+			.from('Players')
+			.insert([
+				subscription,
+			])
+		
+		return !error ? data : error
+	})
+	app.hook('notifications:subscribers', async () => {
+		const { data, error } = await supabase
+			.from('Players')
+			.select()
+		
+			return !error ? data : error
+	})
+	app.hook('notifications:unsubscribe', async (subscription) => {
+		const { data, error } = await supabase
+			.from('Players')
+			.delete()
+			.match(subscription)
+	})
+
+	app.hook('biketag:newtag', (subscription) => {
+		app.hook('notifications:notify', {subscription, title: 'A new tag has been played!', message: 'from something'})
+	})
+}
+
 const app = sexpress({
     onPageDataRequest,
     onConfigurationLoad,
+	onLoad,
 })
 
 app.run(appStarted)
